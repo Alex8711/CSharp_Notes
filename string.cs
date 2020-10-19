98c0dbb157abb7b267a52d9b61bd9f34b9aa0366
Alex8711 <liuzhendell@gmail.com>
2020-10-19 10:28:26 -0600

    some notes

---
+++ string.cs
@@ -0,0 +1,8 @@
+//Indicates whether the specified string is null or an empty string
+// true if the value parameter is null or an empty string (""); otherwise, false.
+string.IsNullOrEmpty(string value);
+
+//Indicates whether a specified string is null, empty, or consists only white-space characters.
+//true if the value parameter is null or Empty, or if value consists exclusively of white-space characters.
+string.IsNullOrWhiteSpace(string value);
+
